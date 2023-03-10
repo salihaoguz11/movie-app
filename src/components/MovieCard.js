981e795dfb7b8e38a63e8e73946db36363c9d8b4
Saliha <salihaoguz2011@gmail.com>
2023-03-10 10:43:54 +0000

    started

---
+++ src/components/MovieCard.js
@@ -0,0 +1,5 @@
+const MovieCard = () => {
+  return <div>movie Card</div>;
+};
+
+export default MovieCard;
---
+++ src/pages/MovieDetail.js
@@ -0,0 +1,5 @@
+const MovieDetail = () => {
+  return <div>movie details</div>;
+};
+
+export default MovieDetail;
